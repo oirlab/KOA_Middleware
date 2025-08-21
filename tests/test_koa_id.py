4f9506085260c12b41f0135022ab8f67de17e957
Andrea Zonca <code@andreazonca.com>
2025-08-21 12:21:34 -0700

    feat: Add minimal KOA Middleware integration test and generalize test suite

--- tests/test_koa_id.py
+++ tests/test_koa_id.py
@@ -16,4 +16,4 @@ def test_generate_koa_filepath():
     date_obs = '20240924'
     utc_obs = '12:34:56.78'
     koa_filepath = generate_koa_filepath(instrument, instrument_prefix, data_level, date_obs, utc_obs)
-    assert koa_filepath == '/HISPEC/2024/20240924/L1/HB.20240924.45296.78.fits'
+    assert koa_filepath == '/TEST_INSTRUMENT/2024/20240924/L1/HB.20240924.45296.78.fits'
